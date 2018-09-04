cbd91c576cbbdae37247a19556e74038d4daaad0
Unknown <iltaekkwon@gmail.com>
2018-09-04 10:23:14 +0900

    Strategy Pattern
    - 알고리즘을 정의하고, 각각을 캡슐화하여 교환해서 사용할 수 있도록 만듦.
    - 알고리즘을 사용하는 클라이언트와는 독립적으로 알고리즘을 변경할 수 있음

---
+++ designPattern/src/StrategyPattern/ModelDuck.java
@@ -0,0 +1,13 @@
+package StrategyPattern;
+
+public class ModelDuck extends Duck {
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am model duck!");
+    }
+}
