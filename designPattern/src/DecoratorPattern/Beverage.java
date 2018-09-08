1a480a5de6691b048a3f2c5d2b769ecc5821807a
Unknown <iltaekkwon@gmail.com>
2018-09-08 18:55:34 +0900

    update Decorator Pattern
    객체에 추가 요소를 동적으로 더할 수 있는 디자인 패턴.

---
+++ designPattern/src/DecoratorPattern/Beverage.java
@@ -0,0 +1,21 @@
+package DecoratorPattern;
+
+public abstract class Beverage {
+    String description = "no name";
+    public enum SIZES {TALL, GRANDE, VENTI};
+    SIZES size = SIZES.TALL;
+
+    public void setSize(SIZES size) {
+        this.size = size;
+    }
+
+    public SIZES getSize() {
+        return this.size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
+}
---
+++ designPattern/src/DecoratorPattern/SteamMilk.java
@@ -0,0 +1,32 @@
+package DecoratorPattern;
+
+public class SteamMilk extends CondimentDecorator {
+    Beverage beverage;
+
+    @Override
+    public SIZES getSize() {
+        return beverage.getSize();
+    }
+
+    public SteamMilk(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", SteamMilk";
+    }
+
+    @Override
+    public double cost() {
+        double cost = beverage.cost();
+        if (getSize() == SIZES.TALL) {
+            cost += .10;
+        } else if (getSize() == SIZES.GRANDE) {
+            cost += .15;
+        } else if (getSize() == SIZES.VENTI) {
+            cost += .20;
+        }
+        return cost;
+    }
+}
